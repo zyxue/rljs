b6ef28a101c641c5244d0ab4186df4869d8d82a8
Zhuyi Xue <alfred532008@gmail.com>
2016-10-16 10:13:03 -0700

    copied over GridWorldDP/* to GridWorldTD/

---
+++ src/modules/GridWorldTD/Grid.jsx
@@ -0,0 +1,173 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactFauxDOM from 'react-faux-dom';
+import * as d3 from 'd3';
+
+
+/* draws the grid based on agent.env, agent.V and agent.P */
+
+class Grid extends React.Component {
+    initGrid (fauxElement, agent) {
+        let env = agent.env;
+        let V = agent.V;
+        let P = agent.P
+
+        let d3elt = d3.select(fauxElement);
+        /* d3elt.append('div').html('');*/
+
+        let cs = 60; /* cell size */
+
+        var gh = env.gh; // height in cells
+        var gw = env.gw; // width in cells
+        var gs = env.gs; // total number of cells
+
+        let w = 600;
+        let h = 600;
+        let svg = d3elt.append('svg')
+                       .attr('width', w)
+                       .attr('height', h)
+                       .append('g')
+                       .attr('transform', 'scale(1)');
+
+        // define a marker for drawing arrowheads
+        svg.append("defs")
+           .append("marker")
+           .attr("id", "arrowhead")
+           .attr("refX", 3)
+           .attr("refY", 2)
+           .attr("markerWidth", 3)
+           .attr("markerHeight", 4)
+           .attr("orient", "auto")
+           .append("path")
+           .attr("d", "M 0,0 V 4 L3,2 Z");
+
+
+        for (var y = 0; y < gh; y++) {
+            for (var x = 0; x < gw; x++) {
+                var xcoord = x * cs;
+                var ycoord = y * cs;
+                var r = 255,
+                    g = 255,
+                    b = 255;
+                var s = env.xytos(x, y);
+
+                var grp = svg.append('g');
+
+
+                // click callbackfor group
+                grp.on('click', function(ss) {
+                    /* return function() { cellClicked(ss); } // close over s*/
+                }(s));
+
+
+                var vv = V[s];
+                var ms = 100;
+                if (vv > 0) {
+                    g = 255;
+                    r = 255 - vv * ms;
+                    b = 255 - vv * ms;
+                }
+                if (vv < 0) {
+                    g = 255 + vv * ms;
+                    r = 255;
+                    b = 255 + vv * ms;
+                }
+
+                /* value color */
+                var vcol;
+                if (env.T[s] === 1) {
+                    vcol = "#AAA";
+                } else {
+                    vcol = 'rgb(' + Math.floor(r) + ',' + Math.floor(g) + ',' + Math.floor(b) + ')';
+                }
+
+                // set up cell rectangles
+                grp.append('rect')
+                   .attr('x', xcoord)
+                   .attr('y', ycoord)
+                   .attr('height', cs)
+                   .attr('width', cs)
+                   .attr('fill', vcol)
+                   .attr('stroke', 'black')
+                   .attr('stroke-width', 2);
+
+                // state text
+                grp.append('text')
+                   .attr('x', xcoord + cs - 15)
+                   .attr('y', ycoord + 10)
+                   .attr('font-size', 10)
+                   .text(s.toFixed(0));
+
+                // skip rest for cliffs
+                if (env.T[s] === 1) {
+                    continue;
+                }
+
+                // reward text
+                grp.append('text')
+                   .attr('x', xcoord + 3)
+                   .attr('y', ycoord + 10)
+                   .attr('font-size', 10)
+                   .text(env.Rarr[s].toFixed(1.1));
+
+                // value text
+                grp.append('text')
+                   .attr('x', xcoord + 3)
+                   .attr('y', ycoord + cs - 5)
+                   .attr('font-size', 10)
+                   .text(V[s].toFixed(2));
+
+                // this highlights where the (0, 0) point is: top left corner
+                /* grp.append('circle')
+                 *  .attr('cx', xcoord)
+                 *  .attr('cy', ycoord)
+                 *  .attr('fill', 'red')
+                 *  .attr('r', 4);*/
+
+                // policy arrows
+                for (var a = 0; a < 4; a++) {
+                    var pa = grp.append('line');
+
+                    var prob = P[a * gs + s];
+                    if (prob === 0) {
+                        pa.attr('visibility', 'hidden');
+                    } else {
+                        pa.attr('visibility', 'visible');
+                    }
+                    var nx, ny;
+                    /* var ss = cs / 2 * prob * 0.9;*/
+                    var ss = cs / 6;
+                    if (a === 0) {nx = -ss; ny = 0;}
+                    if (a === 1) {nx = 0; ny = -ss;}
+                    if (a === 2) {nx = 0; ny = ss;}
+                    if (a === 3) {nx = ss; ny = 0;}
+
+                    /* mapping are as below */
+                    /* var actionMapping = {
+                     *     0: '←',
+                     *     1: '↑',
+                     *     2: '↓',
+                     *     3: '→'
+                     * };*/
+
+                    pa.attr('x1', xcoord + cs / 2)
+                      .attr('y1', ycoord + cs / 2)
+                      .attr('x2', xcoord + cs / 2 + nx)
+                      .attr('y2', ycoord + cs / 2 + ny)
+                      .attr('stroke', 'black')
+                      .attr('stroke-width', '1')
+                      .attr("marker-end", "url(#arrowhead)");
+                }
+            }
+        }
+    }
+
+    render () {
+        var el = ReactFauxDOM.createElement('div');
+        this.initGrid(el, this.props.agent);
+        return el.toReact();
+    }
+}
+
+
+export default Grid;
---
+++ src/modules/GridWorldTD/GridWorldEnv.js
@@ -0,0 +1,160 @@
+import {
+    R,
+    RL
+} from '../..//lib/rl.js';
+
+
+var GridWorld = function() {
+    this.Rarr = null; // reward array
+    this.T = null; // cell types, 0 = normal, 1 = cliff
+    this.reset();
+};
+
+
+GridWorld.prototype = {
+    reset: function() {
+
+        // hardcoding one gridworld for now
+        this.gh = 10;
+        this.gw = 10;
+        this.gs = this.gh * this.gw; // number of states
+
+        // specify some rewards
+        let Rarr = R.zeros(this.gs);
+        /* cliffs */
+        let T = R.zeros(this.gs);
+
+        let plusOneIdx = [55];
+        for (let i = 0; i < plusOneIdx.length; i++) {
+            Rarr[plusOneIdx[i]] = 1;
+        }
+
+        let negOneIdx = [54, 64, 65, 85, 86, 37, 33, 67, 57];
+        for (let i = 0; i < negOneIdx.length; i++) {
+            Rarr[negOneIdx[i]] = -1;
+        }
+
+        // make some cliffs
+        for (let q = 0; q < 8; q++) {
+            let off = (q + 1) * this.gh + 2;
+            T[off] = 1;
+            Rarr[off] = 0;
+        }
+
+        for (let q = 0; q < 6; q++) {
+            let off = 4 * this.gh + q + 2;
+            T[off] = 1;
+            Rarr[off] = 0;
+        }
+
+        T[5 * this.gh + 2] = 0;
+        Rarr[5 * this.gh + 2] = 0; // make a hole
+
+        this.Rarr = Rarr;
+        this.T = T;
+
+    },
+
+    reward: function(s, a, ns) {
+        // reward of being in s, taking action a, and ending up in ns
+        return this.Rarr[s];
+    },
+
+    nextStateDistribution: function(s, a) {
+        let ns;
+        // given (s,a) return distribution over s' (in sparse form)
+        if (this.T[s] === 1) {
+            // cliff! oh no!
+            // var ns = 0; // reset to state zero (start)
+        } else if (s === 55) {
+            // agent wins! teleport to start
+            ns = this.startState();
+            while (this.T[ns] === 1) {
+                ns = this.randomState();
+            }
+        } else {
+            // ordinary space
+            let nx, ny;
+            let x = this.stox(s);
+            let y = this.stoy(s);
+            if (a === 0) {nx = x - 1; ny = y;}
+            if (a === 1) {nx = x; ny = y - 1;}
+            if (a === 2) {nx = x; ny = y + 1;}
+            if (a === 3) {nx = x + 1; ny = y;}
+            ns = nx * this.gh + ny;
+            if (this.T[ns] === 1) {
+                // actually never mind, this is a wall. reset the agent
+                ns = s;
+            }
+        }
+        // gridworld is deterministic, so return only a single next state
+        return ns;
+    },
+
+    sampleNextState: function(s, a) {
+        // gridworld is deterministic, so this is easy
+        let ns = this.nextStateDistribution(s, a);
+        let r = this.Rarr[s]; // observe the raw reward of being in s, taking a, and ending up in ns
+        r -= 0.01; // every step takes a bit of negative reward
+        let out = {
+            'ns': ns,
+            'r': r
+        };
+        if (s === 55 && ns === 0) {
+            // episode is over
+            out.reset_episode = true;
+        }
+        return out;
+    },
+
+    allowedActions: function(s) {
+        let x = this.stox(s);
+        let y = this.stoy(s);
+        let as = [];
+        if (x > 0) {
+            as.push(0);
+        }
+        if (y > 0) {
+            as.push(1);
+        }
+        if (y < this.gh - 1) {
+            as.push(2);
+        }
+        if (x < this.gw - 1) {
+            as.push(3);
+        }
+        return as;
+    },
+
+    randomState: function() {
+        return Math.floor(Math.random() * this.gs);
+    },
+
+    startState: function() {
+        return 0;
+    },
+
+    getNumStates: function() {
+        return this.gs;
+    },
+
+    getMaxNumActions: function() {
+        return 4;
+    },
+
+    // private functions
+    stox: function(s) {
+        return Math.floor(s / this.gh);
+    },
+
+    stoy: function(s) {
+        return s % this.gh;
+    },
+
+    xytos: function(x, y) {
+        return x * this.gh + y;
+    }
+};
+
+
+export default GridWorld;
