e31e72c777583309dbe4851047d4f12d326b47a4
Zhuyi Xue <alfred532008@gmail.com>
2017-01-08 23:20:41 -0800

    added TDControl/Grid.jsx inherited from Base/Grid.jsx

---
+++ src/examples/GridWorld/TDControl/Grid.jsx
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+import * as d3 from 'd3';
+
+
+import GridBase from '../Base/Grid.jsx';
+
+
+class Grid extends GridBase {
+    drawCells(context) {
+        
+    }
+}
+
+export default Grid;
