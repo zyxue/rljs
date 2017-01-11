f59a10cffb575dbb1df1d9c430b5c4090744e6cb
Zhuyi Xue <alfred532008@gmail.com>
2017-01-11 14:59:41 -0800

    calculated geometrical dimensions for each cell upon grid initialization

--- src/examples/GridWorld/Base/Grid.jsx
+++ src/examples/GridWorld/Base/Grid.jsx
@@ -290,6 +290,23 @@ class Grid extends Component {
      *                   .attr('d', this.arc());
      * }*/
 
+    addDimensionsToStates() {
+        // assign geometrical dimensions to each state according to passed
+        // height and width
+
+        const {height, width, agent} = this.props;
+        const {numRows, numCols} = agent.env;
+        const cellHeight = height / numRows;
+        const cellWidth = width  / numCols;
+
+        let that = this;
+        agent.env.states.forEach(function (st) {
+            st.cellHeight = cellHeight;
+            st.cellWidth = cellWidth;
+            st.coords = that.calcCoords(st.x, st.y, cellHeight, cellWidth);
+        });
+    }
+
     propTypes: {
         id: PropTypes.string,
         height: PropTypes.number,
@@ -297,6 +314,7 @@ class Grid extends Component {
     }
 
     componentDidMount() {
+        this.addDimensionsToStates();
         this.drawGrid();
     }
 
