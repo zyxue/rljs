0a238c3dd1e22cc302809dd1c39d80320e742071
Zhuyi Xue <alfred532008@gmail.com>
2017-01-11 16:09:24 -0800

    ported essential features to src/examples/GridWorld/TDControl/Grid.jsx

--- src/examples/GridWorld/TDControl/Grid.jsx
+++ src/examples/GridWorld/TDControl/Grid.jsx
@@ -7,14 +7,14 @@ import GridBase from '../Base/Grid.jsx';
 
 class Grid extends GridBase {
     drawCells(context) {
-        const {agent, showLegend} = this.props;
+        const {agent, legendsCtrl} = this.props;
 
         let that = this;
         agent.env.states.forEach(function (st) {
             let grp = context.append('g');
             let fillColor = st.isCliff ? '#AAA' : that.genRGBColorString(1);
-            
-            that.drawOneCell(grp, st);
+
+            that.drawOneCell(grp, st, legendsCtrl);
 
             /* grp.append('rect')
              *    .attr('x', st.coords.xmin)
@@ -32,49 +32,35 @@ class Grid extends GridBase {
              *        // console.debug(that.handleMouseClick);
              *        that.handleMouseClick(this, st);
              *    });*/
-            
-            /* if (showLegend.stateValue) that.writeStateValue(grp, st.V, coords);
-             * if (showLegend.stateId) that.writeStateId(grp, st.id, coords);
-             * if (showLegend.stateCoord) that.writeStateCoord(grp, st.x, st.y, coords);
-             * if (showLegend.reward) that.writeReward(grp, st.reward, coords);
-             * 
-             * if (showLegend.etrace) that.drawTrace(grp, st.Z, coords);*/
+
         }) 
         
-        /* if (this.props.selectedState !== null)
-         *     this.highlightState(context, this.props.selectedState, cellHeight, cellWidth,
-         *                         {fillColor: 'orange', fillOpacity: 0.5});
-         * 
-         * this.highlightState(context, agent.env.startingState, cellHeight, cellWidth,
-         *                     {fillColor: 'blue', fillOpacity: 0.3});
-         * this.highlightState(context, agent.env.terminalState, cellHeight, cellWidth,
-         *                     {fillColor: 'green', fillOpacity: 0.3});*/
+        if (this.props.selectedState !== null)
+            this.highlightState(context, this.props.selectedState,
+                                {fillColor: 'orange', fillOpacity: 0.5});
+
+        this.highlightState(context, agent.env.startingState,
+                            {fillColor: 'blue', fillOpacity: 0.3});
+        this.highlightState(context, agent.env.terminalState,
+                            {fillColor: 'green', fillOpacity: 0.3});
     }
 
-    drawOneCell(cellContext, state) {
+    drawOneCell(cellContext, state, legendsCtrl) {
+        let lc = legendsCtrl;
+
         /* let greedyAction = null;
          * let greedyQVal = null;*/
         let that = this;
         state.allowedActions.forEach(function(action) {
-            /* let qVal = Q[currState * maxNumActions + currAction];
-
-             * let needUpdate = false;
-             * if (greedyAction === null) {
-             *     needUpdate = true;
-             * } else {
-             *     if (qVal  > greedyQVal) {
-             *         needUpdate = true;
-             *     }
-             * }
-
-             * if (needUpdate) {
-             *     greedyAction = currAction;
-             *     greedyQVal = qVal;
-             * }*/
-
             that.drawQTriangle(cellContext, state, action);
-            /* if (showQVals) this.writeQ(cellContext, currAction, qVal, coords);*/
+            if (lc.qValue) that.writeQ(cellContext, state, action);
+            if (lc.policy) that.drawPolicyArrow(cellContext, state, action);
+            if (lc.etrace) that.drawTrace(cellContext, state, action);
         })
+
+        if (lc.stateId) this.writeStateId(cellContext, state);
+        if (lc.stateCoord) this.writeStateCoord(cellContext, state);
+        if (lc.reward) this.writeReward(cellContext, state);
     }
 
     genQPointsStr(coords, action) {
@@ -118,110 +104,43 @@ class Grid extends GridBase {
                    .attr('stroke-width', 0.5);
     }
 
-        // draw arrow for greedy action
-        /* if (greedyQVal !== 0) {
-         *     let nx, ny;
-         *     let scaler = 4;
-         *     if (greedyAction === 0) {nx = - cellWidth / scaler; ny = 0;}
-         *     if (greedyAction === 1) {nx = 0; ny = - cellHeight / scaler;}
-         *     if (greedyAction === 2) {nx = cellWidth / scaler; ny = 0;}
-         *     if (greedyAction === 3) {nx = 0; ny = cellHeight / scaler;}
+    drawTrace(cellContext, state, action) {
+        let Z = state.Z[action];
+        let {xmin, ymin,  xmid, ymid, xmax, ymax} = state.coords;
+        let cx, cy;
+        let C = 1 / 3;
 
-         *     let pa = cellContext.append('line')
-         *                         .attr('x1', coords.xmid)
-         *                         .attr('y1', coords.ymid)
-         *                         .attr('x2', coords.xmid + nx)
-         *                         .attr('y2', coords.ymid + ny)
-         *                         .attr('stroke', 'black')
-         *                         .attr('stroke-width', 1.5)
-         *                         .attr("marker-end", "url(#arrowhead)");
-         * }*/
+        if (action === 0) {
+            cx = (1 - C) * xmin + C * xmid;
+            cy = ymid;
+        } else if (action === 1) {
+            cx = xmid;
+            cy = (1 - C) * ymin + C * ymid;
+        } else if (action === 2) {
+            cx = C * xmid + (1 - C) * xmax;
+            cy = ymid;
+        } else if (action === 3) {
+            cx = xmid;
+            cy = C * ymid + (1 - C) * ymax;
+        }
 
-    /* if (showStateVals) this.writeState(cellContext, currState, coords);
-     * if (showStateCoords) this.writeStateCoord(cellContext, ci, ri, coords);
-     * // this just show the reward of entering a given state without
-     * // per step penalty
-     * if (showRewardVals) this.writeReward(cellContext, env.Rarr[currState], coords);
-       }*/
+        cellContext.append('circle')
+               .attr('cx', cx)
+               .attr('cy', cy)
+               // log so that size of circle doesn't change too dramatically
+               // among neighbouring cells, 1 to avoid log of 0, and negative radius
+        /* .attr('r', Math.log(Z * 1000 + 1))*/
+                   .attr('r', Math.log(0.1 * 1000 + 1))
+               .attr('fill', '#FF0')
+               .attr('fill-opacity', 1)
+               .attr('stroke', '#000')
+               .attr('id', 'cpos');
+    }
 
 
-    /* 
-     *     drawOneCell(cellContext, env, Q, maxNumActions, cellHeight, cellWidth,
-     *                 ci, ri, coords,
-     *                 showQTriangles, showQVals,
-     *                 showStateVals, showStateCoords, showRewardVals) {
-     *         let currState = env.xytos(ci, ri);
-     *         
-     *         if (showQTriangles) {
-     *             let allowedActions = env.getAllowedActions(currState);
-     * 
-     *             let greedyAction = null;
-     *             let greedyQVal = null;
-     *             for(let i=0; i < allowedActions.length; i++) {
-     *                 let currAction = allowedActions[i];
-     *                 let qVal = Q[currState * maxNumActions + currAction];
-     * 
-     *                 let needUpdate = false;
-     *                 if (greedyAction === null) {
-     *                     needUpdate = true;
-     *                 } else {
-     *                     if (qVal  > greedyQVal) {
-     *                         needUpdate = true;
-     *                     }
-     *                 }
-     * 
-     *                 if (needUpdate) {
-     *                     greedyAction = currAction;
-     *                     greedyQVal = qVal;
-     *                 }
-     * 
-     *                 this.drawQTriangle(cellContext, currAction, coords, {qVal: qVal});
-     *                 if (showQVals) this.writeQ(cellContext, currAction, qVal, coords);
-     *             }
-     * 
-     *             // draw arrow for greedy action
-     *             if (greedyQVal !== 0) {
-     *                 let nx, ny;
-     *                 let scaler = 4;
-     *                 if (greedyAction === 0) {nx = - cellWidth / scaler; ny = 0;}
-     *                 if (greedyAction === 1) {nx = 0; ny = - cellHeight / scaler;}
-     *                 if (greedyAction === 2) {nx = cellWidth / scaler; ny = 0;}
-     *                 if (greedyAction === 3) {nx = 0; ny = cellHeight / scaler;}
-     * 
-     *                 let pa = cellContext.append('line')
-     *                                     .attr('x1', coords.xmid)
-     *                                     .attr('y1', coords.ymid)
-     *                                     .attr('x2', coords.xmid + nx)
-     *                                     .attr('y2', coords.ymid + ny)
-     *                                     .attr('stroke', 'black')
-     *                                     .attr('stroke-width', 1.5)
-     *                                     .attr("marker-end", "url(#arrowhead)");
-     *             }
-     *         } else {
-     *             drawRect(cellContext, coords.xmin, coords.ymin, cellHeight, cellWidth);
-     *         }
-     * 
-     *         if (showStateVals) this.writeState(cellContext, currState, coords);
-     *         if (showStateCoords) this.writeStateCoord(cellContext, ci, ri, coords);
-     *         // this just show the reward of entering a given state without
-     *         // per step penalty
-     *         if (showRewardVals) this.writeReward(cellContext, env.Rarr[currState], coords);
-     *     }
-     * 
-     *     drawQTriangle(cellContext, action, coords, {qVal=0, color=genRGBColorString(qVal)}={}) {
-     *         let pointsStr = genPointsStr(action, coords);
-     *         // console.debug(pointsStr);
-     *         cellContext.append('polygon')
-     *                    .attr('points', pointsStr)
-     *                    .attr('fill', color)
-     *                    .attr('fill-opacity', 1)
-     *                    .attr('stroke', 'black')
-     *                    .attr('stroke-width', 0.5);
-     *     }
-     * */
-
-    writeQ(cellContext, action, qval, coords) {
-        let {xmin, ymin,  xmid, ymid, xmax, ymax} = coords;
+    writeQ(cellContext, state, action) {
+        let {xmin, ymin,  xmid, ymid, xmax, ymax} = state.coords;
+        let qVal = state.Q[action];
         if (action === 0) {
             cellContext.append('text')
                        .attr('x', xmin)
@@ -229,7 +148,7 @@ class Grid extends GridBase {
                        .attr('font-size', 10)
                        .attr("text-anchor", "start")
                        .attr("dominant-baseline", "central")
-                       .text(qval.toFixed(1));
+                       .text(qVal.toFixed(1));
 
         } else if (action === 1) {
             cellContext.append('text')
@@ -238,7 +157,7 @@ class Grid extends GridBase {
                        .attr('font-size', 10)
                        .attr("text-anchor", "middle")
                        .attr("dominant-baseline", "text-before-edge")
-                       .text(qval.toFixed(1));
+                       .text(qVal.toFixed(1));
         } else if (action === 2) {
             cellContext.append('text')
                        .attr('x', xmax)
@@ -246,7 +165,7 @@ class Grid extends GridBase {
                        .attr('font-size', 10)
                        .attr("text-anchor", "end")
                        .attr("dominant-baseline", "central")
-                       .text(qval.toFixed(1));
+                       .text(qVal.toFixed(1));
 
         } else if (action === 3) {
             cellContext.append('text')
@@ -255,7 +174,7 @@ class Grid extends GridBase {
                        .attr('font-size', 10)
                        .attr("text-anchor", "middle")
                        .attr("dominant-baseline", "text-after-edge")
-                       .text(qval.toFixed(1));
+                       .text(qVal.toFixed(1));
         }
     }
 
