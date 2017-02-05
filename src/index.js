7021592bbc37f9e396079bbfd5d3bc3ad686612f
Zhuyi Xue <alfred532008@gmail.com>
2017-02-04 20:58:24 -0800

    renamed GridWorldTD.jsx to TDControlView

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import GridWorldDP from './examples/GridWorldDP.jsx';
 import GridWorldTDPred from './examples/GridWorldTDPred.jsx';
-import GridWorldTD from './examples/GridWorldTD.jsx';
+import TDControlView from './examples/GridWorld/TDControl/View.jsx';
 
 
 ReactDOM.render((
@@ -16,7 +16,7 @@ ReactDOM.render((
         <Route path="/" component={App}>
             <Route path="/gridworld-dp" component={GridWorldDP}/>
         <Route path="/gridworld-td-pred" component={GridWorldTDPred}/>
-        <Route path="/gridworld-td" component={GridWorldTD}/>
+        <Route path="/gridworld-td" component={TDControlView}/>
         </Route>
     </Router>
 ), document.getElementById('root'));
