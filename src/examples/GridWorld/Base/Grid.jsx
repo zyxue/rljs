59706270bf27eb050db2723e505ef73fb5b407d4
Zhuyi Xue <alfred532008@gmail.com>
2017-02-19 12:09:05 -0800

    cleaned src/examples/GridWorld/Base/Grid.jsx

--- src/examples/GridWorld/Base/Grid.jsx
+++ src/examples/GridWorld/Base/Grid.jsx
@@ -6,53 +6,11 @@ import Cell from './Cell.jsx';
 import Cliff from './Cliff.jsx';
 import {calcCoords} from './gridUtils.js';
 
-/* adapted from
-https://github.com/alanbsmith/react-d3-example/blob/master/src/ProgressArc.js */
-
 
 class Grid extends Component {
-    /* drawGrid() {
-     *     const context = this.setContext();
-     *     this.addDimensionsToStates();
-     *     this.drawCells(context);
-     *     this.drawAgent(context);
-     * }
-
-     * redrawCell() {
-     *     const context = d3.select('#' + this.props.id);
-     *     context.remove();
-     *     this.drawGrid();
-     * }
-     */
-
-    /* setContext() {
-     *     const {height, width, id} = this.props;
-     *     let context = d3.select(this.refs.gridDiv).append('svg')
-     *                     .attr('height', height)
-     *                     .attr('width', width)
-     *                     .attr('id', id)
-     *                     .append('g');
-
-     *     context.append("defs")
-     *            .append("marker")
-     *            .attr("id", "arrowhead")
-     *            .attr("refX", 3)
-     *            .attr("refY", 2)
-     *            .attr("markerWidth", 3)
-     *            .attr("markerHeight", 4)
-     *            .attr("orient", "auto")
-     *            .append("path")
-     *            .style('stroke', 'black')
-     *            .style('fill', 'black')
-     *            .style('stroke-linejoin', "miter")
-     *            .attr("d", "M 0,0 V 4 L3,2 Z");
-     *     return context;
-     * }*/
-
     addDimensionsToStates() {
         // assign geometrical dimensions to each state according to passed
         // height and width
-
         const {height, width, agent} = this.props;
         const {numRows, numCols} = agent.env;
         const cellHeight = height / numRows;
@@ -72,15 +30,6 @@ class Grid extends Component {
         width: PropTypes.number
     }
 
-    /* 
-     *     componentDidMount() {
-     *         this.drawGrid();
-     *     }
-     * 
-     *     componentDidUpdate() {
-     *         this.redrawGrid();
-     *     }*/
-
     render () {
         const {height, width} = this.props;
         const env = this.props.agent.env;
@@ -89,13 +38,15 @@ class Grid extends Component {
             return (
                 state.isCliff ?
                 <Cliff key={state.id} state={state} /> :
-                <Cell key={state.id} state={state} />
-            )
+                <Cell  key={state.id} state={state} />
+            );
         });
 
         return (
             <div>
-                <svg height={height} width={width}>{grid}</svg>
+                <svg height={height} width={width}>
+                    {grid}
+                </svg>
             </div>
         );
     }
