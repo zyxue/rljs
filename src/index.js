670160ceb52667dbba4475a1a738f26a0aca643d
Zhuyi Xue <alfred532008@gmail.com>
2017-02-22 20:49:26 -0800

    restructured index.js

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 import App from './App';
-import GridWorldDP from './examples/GridWorldDP.jsx';
-import GridWorldTDPred from './examples/GridWorldTDPred.jsx';
-import TDControlView from './examples/GridWorld/TDControl/View.jsx';
-import DynaQView from './examples/GridWorld/DynaQ/View.jsx';
+import GridWorldDPView        from './examples/GridWorld/DP/View.jsx';
+import GridWorldTDView        from './examples/GridWorld/TD/View.jsx';
+import GridWorldTDControlView from './examples/GridWorld/TDControl/View.jsx';
+import GridWorldDynaQView     from './examples/GridWorld/DynaQ/View.jsx';
 
 
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={App}>
-            <Route path="/gridworld-dp" component={GridWorldDP} />
-            <Route path="/gridworld-td-pred" component={GridWorldTDPred}/>
-            <Route path="/gridworld-td-ctrl" component={TDControlView}/>
-            <Route path="/gridworld-dyna-q" component={DynaQView}/>
+        <Route path="/gridworld-dp" component={GridWorldDPView} />
+        <Route path="/gridworld-td-pred" component={GridWorldTDView}/>
+        <Route path="/gridworld-td-ctrl" component={GridWorldTDControlView}/>
+        <Route path="/gridworld-dyna-q" component={GridWorldDynaQView}/>
         </Route>
     </Router>
 ), document.getElementById('root'));
