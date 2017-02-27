ab1229ba1bd5ea11d48b7a660eed60f8def037ae
Zhuyi Xue <alfred532008@gmail.com>
2017-02-27 13:10:37 -0800

    modularized agentBtns.jsx

---
+++ src/examples/GridWorld/DP/agentBtns.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {Col, Button, ButtonToolbar} from 'react-bootstrap';
+
+
+class AgentButtons extends Component {
+    render() {
+        const btnData = [
+            ['evalPolOneSweep', 'Policy evaluation (one sweep)'],
+            ['evalPol', 'Policy evaluatation (till convergence)'],
+            ['togglePolEval', 'Policy evaluation toggle'],
+            ['toggleValFuncOptim', 'Value function optimization toggle'],
+            ['updatePol', 'Update policy'],
+            ['polIter', 'Policy iteration'],
+            ['vallIter', 'Value iteration'],
+            ['reset', 'Reset'],
+        ];
+
+        const btns = btnData.map(([key, label]) => {
+            return (
+                <Button key={key}
+                        bsStyle='primary'
+                        bsSize='xsmall'
+                        onClick={this.props.handleClick.bind(this, key)}>
+                    {label}
+                </Button>
+            );
+        });
+
+        return (
+            <ButtonToolbar className="wrapped-buttons">
+                {btns}
+            </ButtonToolbar>
+        );
+    }
+}
+
+
+export default AgentButtons;
