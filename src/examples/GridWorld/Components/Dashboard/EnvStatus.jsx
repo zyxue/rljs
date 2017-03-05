70daeba8134a6a4d64bbf3dd5a0ca6992df471c4
Zhuyi Xue <alfred532008@gmail.com>
2017-03-05 14:12:50 -0800

    use columns instead of cols in Components/Dashboard/EnvStatus.jsx

--- src/examples/GridWorld/Components/Dashboard/EnvStatus.jsx
+++ src/examples/GridWorld/Components/Dashboard/EnvStatus.jsx
@@ -27,7 +27,7 @@ EnvStatus.defaultProps = {
     envParamSpecs: [
         {
             specType: 'select',
-            label: '# rows',
+            label: '# rows: ',
             attr: 'numRows',
             spec: {
                 options: [3, 4, 5, 6, 7, 8, 9, 10].map((v) => {return {value:v, label: v};})
@@ -35,7 +35,7 @@ EnvStatus.defaultProps = {
         },
         {
             specType: 'select',
-            label: '# cols',
+            label: '# columns: ',
             attr: 'numCols',
             spec: {
                 options: [3, 4, 5, 6, 7, 8, 9, 10].map((v) => {return {value:v, label: v};})
