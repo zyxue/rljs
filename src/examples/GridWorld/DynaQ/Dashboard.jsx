92f9ab3533ce7d3420839ba520b7acfd53137864
Zhuyi Xue <alfred532008@gmail.com>
2017-03-05 13:30:01 -0800

    eslint DynaQ/Dashboard.jsx

--- src/examples/GridWorld/DynaQ/Dashboard.jsx
+++ src/examples/GridWorld/DynaQ/Dashboard.jsx
@@ -1,7 +1,5 @@
-import React, {Component} from 'react';
-import {Row, Col, Button, ButtonToolbar} from 'react-bootstrap';
-
 import Dashboard from '../TDCtrl/Dashboard.jsx';
+
 
 class DynaQDashboard extends Dashboard {
     constructor(props) {
