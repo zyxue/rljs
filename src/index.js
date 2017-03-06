6c896665e9a8a4dad2f482780efcf444116546b6
Zhuyi Xue <alfred532008@gmail.com>
2017-03-05 19:47:32 -0800

    integrated google analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
+import ReactGA from 'react-ga';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -12,13 +13,42 @@ import GridWorldTDCtrlView from './examples/GridWorld/TDCtrl/View.jsx';
 import GridWorldDynaQView     from './examples/GridWorld/DynaQ/View.jsx';
 
 
-ReactDOM.render((
-    <Router history={hashHistory}>
+const router = (
+
         <Route path="/" component={App}>
+            <Route path="/gridworld-dp" component={GridWorldDPView} />
+            <Route path="/gridworld-td-pred" component={GridWorldTDView}/>
+            <Route path="/gridworld-td-ctrl" component={GridWorldTDCtrlView}/>
+            <Route path="/gridworld-dyna-q" component={GridWorldDynaQView}/>
+        </Route>
+    </Router>
+);
+
+const routes = (
+    <Route path="/" component={App}>
         <Route path="/gridworld-dp" component={GridWorldDPView} />
         <Route path="/gridworld-td-pred" component={GridWorldTDView}/>
         <Route path="/gridworld-td-ctrl" component={GridWorldTDCtrlView}/>
         <Route path="/gridworld-dyna-q" component={GridWorldDynaQView}/>
-        </Route>
-    </Router>
-), document.getElementById('root'));
+    </Route>
+);
+
+
+const app = document.getElementById('root');
+
+if (process.env.NODE_ENV === 'production') {
+    ReactGA.initialize(process.env.REACT_APP_GA_ID);
+
+    function logPageView() {
+        ReactGA.set({ page: window.location.pathname });
+        ReactGA.pageview(window.location.pathname);
+    }
+
+    ReactDOM.render(
+        <Router history={hashHistory} routes={routes} onUpdate={logPageView} />,
+        app);
+} else {
+    ReactDOM.render(
+        <Router history={hashHistory} routes={routes} />,
+        app);
+}
