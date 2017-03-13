5bcead0ccaa2a30410bf16866869429476e7502d
Zhuyi Xue <alfred532008@gmail.com>
2017-03-13 11:36:59 -0700

    refactored rounter setup with react-router-dom@4.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,55 @@
 import React from 'react';
-import { Link } from 'react-router';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, hashHistory } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
-import './App.css';
-
-
-const NavLink = React.createClass({
-    render() {
-        return <Link {...this.props} activeClassName="active"/>
-    }
-});
+import GridWorldDPView from './examples/GridWorld/DP/View.jsx';
+import GridWorldTDPredView from './examples/GridWorld/TDPred/View.jsx';
+import GridWorldTDCtrlView from './examples/GridWorld/TDCtrl/View.jsx';
+import GridWorldDynaQView from './examples/GridWorld/DynaQ/View.jsx';
 
 
-export default React.createClass({
-    render() {
-        return (
-            <div>
-                <h1>RLjs examples</h1>
-                <nav>
-                    <ul role="navigation" className="nav nav-pills">
-                        <li><NavLink to="/gridworld-dp">Dynamic Programming</NavLink></li>
-                        <li><NavLink to="/gridworld-td-pred">TD Prediction</NavLink></li>
-                        <li><NavLink to="/gridworld-td-ctrl">TD Control</NavLink></li>
-                        <li><NavLink to="/gridworld-dyna-q">Dyna-Q</NavLink></li>
-                    </ul>
-                </nav>
-                {this.props.children}
+const Home = () => (
+    <div>
+        <h2>Welcome to RLjs</h2>
+        <h4>RLjs currently serves as a playground for Reinforcement learning.</h4>
+        <p>
+            Several most common tabular RL algorithms have been
+            implemented in javascript, and demenstrated with gridword as
+            an toy example. Have fun!
+        </p>
 
-                <h2>Welcome to RLjs</h2>
-                <h4>RLjs currently serves as a playground for Reinforcement learning.</h4>
-                <p>
-                  Several most common tabular RL algorithms have been
-                  implemented in javascript, and demenstrated with gridword as
-                  an toy example. Have fun!
-                </p>
-
-                <p>
-                  If you find anything that does not make sense, feel free
-                  to <a href="https://github.com/zyxue/rljs/issues/new"
+        <p>
+            If you find anything that does not make sense, feel free
+            to <a href="https://github.com/zyxue/rljs/issues/new"
                   target="_blank">open a new issue</a> on github.
-                </p>
-            </div>
-        );
-    }
-});
+        </p>
+    </div>
+);
+
+
+const App = () => (
+    <Router>
+        <div>
+            <h1><Link to="/">RLjs examples</Link></h1>
+            <nav>
+                <ul role="navigation" className="nav nav-pills">
+                    <li><Link to="/gridworld-dp">Dynamic Programming</Link></li>
+                    <li><Link to="/gridworld-td-pred">TD Prediction</Link></li>
+                    <li><Link to="/gridworld-td-ctrl">TD Control</Link></li>
+                    <li><Link to="/gridworld-dyna-q">Dyna-Q</Link></li>
+                </ul>
+            </nav>
+
+
+            <Route exact={true} path="/" component={Home} />
+            <Route path="/gridworld-dp" component={GridWorldDPView} />
+            <Route path="/gridworld-td-pred" component={GridWorldTDPredView}/>
+            <Route path="/gridworld-td-ctrl" component={GridWorldTDCtrlView}/>
+            <Route path="/gridworld-dyna-q" component={GridWorldDynaQView}/>
+        </div>
+    </Router>
+);
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
-import ReactGA from 'react-ga';
+
+import App from './App.jsx';
 
 import 'bootstrap/dist/css/bootstrap.css';
-
-import App from './App';
-import GridWorldDPView        from './examples/GridWorld/DP/View.jsx';
-import GridWorldTDPredView        from './examples/GridWorld/TDPred/View.jsx';
-import GridWorldTDCtrlView from './examples/GridWorld/TDCtrl/View.jsx';
-import GridWorldDynaQView     from './examples/GridWorld/DynaQ/View.jsx';
+import './index.css';
 
 
-const routes = (
-    <Route path="/" component={App}>
-        <Route path="/gridworld-dp" component={GridWorldDPView} />
-        <Route path="/gridworld-td-pred" component={GridWorldTDPredView}/>
-        <Route path="/gridworld-td-ctrl" component={GridWorldTDCtrlView}/>
-        <Route path="/gridworld-dyna-q" component={GridWorldDynaQView}/>
-    </Route>
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
 );
-
-
-const app = document.getElementById('root');
-
-if (process.env.NODE_ENV === 'production') {
-    ReactGA.initialize(process.env.REACT_APP_GA_ID);
-
-    function logPageView() {
-        ReactGA.set({ page: window.location.pathname });
-        ReactGA.pageview(window.location.pathname);
-    }
-
-    ReactDOM.render(
-        <Router history={hashHistory} routes={routes} onUpdate={logPageView} />,
-        app);
-} else {
-    ReactDOM.render(
-        <Router history={hashHistory} routes={routes} />,
-        app);
-}
